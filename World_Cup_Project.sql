98e433b70e722efa6254ab69836e74a347b30ace
Stegen <152444769+StegenX@users.noreply.github.com>
2024-05-10 21:21:55 +0100

    Update World_Cup_Project.sql

--- World_Cup_Project.sql
+++ World_Cup_Project.sql
@@ -1,7 +1,6 @@
 SELECT * 
 FROM world_cup_matches;
 
-
 SELECT year, 
 concat(`Home Team Name`," VS ", `Away Team Name`) AS Matches, 
 concat(`Home Team Goals`, "-", `Away Team Goals`) AS Resultes,
@@ -13,16 +12,26 @@ SELECT year,
 concat(`Home Team Name`," VS ", `Away Team Name`) AS Matches, 
 (`Home Team Goals`+`Away Team Goals`) AS numbers_of_goals_perMatch 
 FROM world_cup_matches
-ORDER BY 3 DESC LIMIT 1;
+ORDER BY 3 DESC LIMIT 10;
 
 
 SELECT year, 
 concat(`Home Team Name`," VS ", `Away Team Name`) AS Matches, Attendance, city 
 FROM world_cup_matches
-ORDER BY 3 DESC LIMIT 1;
+ORDER BY 3 DESC LIMIT 10;
+
+SELECT Year, 
+SUM(Attendance) AS Attendence_Per_Year 
+FROM world_cup_matches
+GROUP BY Year;
 ------------------------------------------------------------------------------
 SELECT * 
 FROM world_cups_summary;
+
+
+UPDATE world_cups_summary
+SET Winner = 'Germany'
+WHERE Winner = 'Germany FR';
 
 SELECT winner, 
 COUNT(Winner) AS number_of_WC_Trophys 
@@ -53,12 +62,10 @@ DROP COLUMN Position;
 
 SELECT Year,  
 concat(`Home Team Name`," VS ", `Away Team Name`) AS Matches,
-GROUP_CONCAT(Event) AS Events 
-FROM world_cup_matches M
+GROUP_CONCAT(Event) AS Events FROM world_cup_matches M
 JOIN world_cup_players P ON P.Matchid = M.MatchID
 WHERE M.Stage = "Final" AND event <> ""
 GROUP BY 1, `Home Team Name`,`Away Team Name`;
-
 
 
 
